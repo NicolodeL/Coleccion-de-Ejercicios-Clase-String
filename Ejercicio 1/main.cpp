f526cf02767650fd5db2561625c57773167e8f4f
NlodeL <nlodelop@myuax.com>
2023-11-23 12:18:11 +0100

    creo el directorio del primer ejercicio, tambien creo main cpp y el cmakelist, en el int main hago el primer apartado

---
+++ Ejercicio 1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+        std::string frase = "Representacion de una frase";
+        std::cout << frase << std::endl;
+        return 0;
+    }
+
+    return 0;
+}
---
+++ cmake-build-debug/CMakeFiles/Ejercicio_1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ejercicio_1.dir/Ejercicio_1/main.cpp.obj"
+  "CMakeFiles/Ejercicio_1.dir/Ejercicio_1/main.cpp.obj.d"
+  "Ejercicio_1.exe"
+  "Ejercicio_1.exe.manifest"
+  "Ejercicio_1.pdb"
+  "libEjercicio_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ejercicio_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
