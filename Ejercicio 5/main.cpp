19377f4b23c6f75ab138b894e0b91afa6cfc9f4b
NlodeL <nlodelop@myuax.com>
2023-11-23 12:58:41 +0100

    intento de modificar el codigo para mejorarlo

--- Ejercicio 5/main.cpp
+++ Ejercicio 5/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 
 int main() {
+
     std::string str = "Inteligencia Artificial";
+    std::cout << "Cadena original: " << str << std::endl;
+
     std::string sub_str = str.substr(13, 9); // "Artificial"
-
-    std::string new_str = str.insert(0, "La "); // "La Inteligencia Artificial"
-
     std::cout << "Subcadena: " << sub_str << std::endl;
-    std::cout << "Cadena resultante: " << new_str << std::endl;
+
+    str.insert(0, "La "); // "La Inteligencia Artificial"
+
+
+    std::cout << "Cadena resultante: " << str << std::endl;
+
 }
