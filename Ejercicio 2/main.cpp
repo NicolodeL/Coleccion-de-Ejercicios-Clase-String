13846785b92f43d3e1a16620f8cde37145779fca
NlodeL <nlodelop@myuax.com>
2023-11-23 12:22:35 +0100

    copio el código proporcionado para ahora  mejorarlo

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 
 int main() {
-
+    std::string sentence = "OpenAI is a leading AI research lab.";
+    std::cout << "Tamaño de la cadena: " << sentence.size() << std::endl;
+    std::cout << "¿Está la cadena vacía? " << (sentence.empty() ? "Sí" : "No") << std::endl;
     return 0;
 }
