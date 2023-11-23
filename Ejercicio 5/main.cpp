fb2d4f777043a0d6f31fe88a04cd509188b4d65b
NlodeL <nlodelop@myuax.com>
2023-11-23 12:53:23 +0100

    copio el codigo del ej5 para mejorarlo

--- Ejercicio 5/main.cpp
+++ Ejercicio 5/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
 int main() {
+    std::string str = "Inteligencia Artificial";
+    std::string sub_str = str.substr(13, 9); // "Artificial"
 
+    std::string new_str = str.insert(0, "La "); // "La Inteligencia Artificial"
+
+    std::cout << "Subcadena: " << sub_str << std::endl;
+    std::cout << "Cadena resultante: " << new_str << std::endl;
 }
