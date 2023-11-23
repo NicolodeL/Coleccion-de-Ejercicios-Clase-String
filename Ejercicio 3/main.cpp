fe716dfabbdec938d5224a3531218a20d5ba1224
NlodeL <nlodelop@myuax.com>
2023-11-23 12:40:40 +0100

    copio el codigo proporcionado del apartado 3 para mejorarlo

--- Ejercicio 3/main.cpp
+++ Ejercicio 3/main.cpp
@@ -2,5 +2,8 @@
 #include <string>
 
 int main() {
-
+    std::string str1 = "Hola";
+    std::string str2 = "mundo!";
+    std::string str3 = str1 + str2;
+    std::cout << "Cadena resultante: " << str3 << std::endl;
 }
