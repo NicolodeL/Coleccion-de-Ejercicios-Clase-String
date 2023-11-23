8328455131a2ef5d3b3dc7b4b98c92ecdc660250
NlodeL <nlodelop@myuax.com>
2023-11-23 12:30:45 +0100

    mejoro el código añadiendo un if para que tambien me diga el valor cuando no esta vacía

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <string>
-
 int main() {
     std::string sentence = "OpenAI is a leading AI research lab.";
-    std::cout << "Tamaño de la cadena: " << sentence.size() << std::endl;
-    std::cout << "¿Está la cadena vacía? " << (sentence.empty() ? "Sí" : "No") << std::endl;
+    std::cout << "Longitud de la cadena: " << sentence.size() << std::endl;
+    std::cout << "\nEsta la cadena vacia?" << std::endl;
+    if (sentence.empty()) {
+        std::cout << "La cadena esta vacia." << std::endl;
+    } else {
+        std::cout << "La cadena no esta vacia y su longitud es: " << sentence.size() << std::endl;
+    }
     return 0;
 }
