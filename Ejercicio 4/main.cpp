71638ae5728eca36fb224501032bbad22c80c9d1
NlodeL <nlodelop@myuax.com>
2023-11-23 12:46:55 +0100

    pongo el código del ej 4 para cambiarlo

--- Ejercicio 4/main.cpp
+++ Ejercicio 4/main.cpp
@@ -2,5 +2,19 @@
 #include <string>
 
 int main() {
+    std::string sentence = "El lenguaje C++ es poderoso.";
+    std::string sub_str = "C++";
 
+    size_t pos = sentence.find(sub_str);
+
+    if(pos != std::string::npos)
+        std::cout << "Subcadena encontrada en la posición: " << pos << std::endl;
+    else
+        std::cout << "Subcadena no encontrada." << std::endl;
+
+    std::string new_sentence = sentence.replace(pos, sub_str.length(), "Python");
+
+    std::cout << "Cadena resultante: " << new_sentence << std::endl;
+
+return 0;
 }
